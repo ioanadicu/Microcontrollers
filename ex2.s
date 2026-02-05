0aa450968a14c739cdf4981878072e93a6e13119
Maria-Ioana Dicu <maria-ioana.dicu@student.manchester.ac.uk>
2026-02-05 16:07:42 +0000

    fix: lb instead of lw for control signals + offset addressing

--- ex2.s
+++ ex2.s
@@ -1,5 +1,5 @@
 START
-    LA a0, LETTER_A 
+    LI a0, LETTER_A 
 
     CALL WAIT_LCD_IDLE
 JAL START
@@ -7,36 +7,38 @@ JAL START
 WAIT_LCD_IDLE
     ; Set to read control with data bus direction as input
     LI a2, 0x9 ; 1001
-    SW a2, LCD_CONTROL, t0
+    LI t0, LCD_DATA
+    SB a2, 1[t0]
 
 STEP_2
     ; Enable signal 1
     LI a2, 0xB ; 1011
-    SW a2, LCD_CONTROL, t0
+    LI t0, LCD_DATA
+    SB a2, 1[t0]
 
     ; Delay to stretch pulse
     LI a2, 0x989690
 LOOP_SUBS1
     SUBI a2, a2, 0x1 
     BNE a2, zero, LOOP_SUBS1
-    RET
 
     ; Read LCD status byte
-    LI a3, LCD_DATA
+    LI t3, LCD_DATA
+    LW a3, [t3]
 
     ; Enable signal 0
     LI a2, 0x9 ; 1001
-    SW a2, LCD_CONTROL, t0
+    LI t0, LCD_DATA
+    SB a2, 1[t0]
 
     ; Delay to separate enable pulses
     LI a2, 0x989690
 LOOP_SUBS2
     SUBI a2, a2, 0x1 
     BNE a2, zero, LOOP_SUBS2
-    RET
 
     ; If bit 7 high repeat from step 2
-    LI a4, MASK_BIT7
+    LI a4, 0x80
     AND a3, a3, a4
     BNEZ a3, STEP_2
 
@@ -46,25 +48,27 @@ WRITE_CHARACTER
 
     ; Set to write data with data bus direction as output
     LI a2, 0x5 ; 0101
-    SW a2, LCD_CONTROL, t0
+    LI t0, LCD_DATA
+    SB a2, 1[t0]
 
     ; Output desired byte
     SW a0, LCD_DATA, t0
 
     ; Enable signal 1
     LI a2, 0x7 ; 0111
-    SW a2, LCD_CONTROL, t0
+    LI t0, LCD_DATA
+    SB a2, 1[t0]
 
     ; Delay to stretch pulse
     LI a2, 0x989690
 LOOP_SUBS3
     SUBI a2, a2, 0x1 
     BNE a2, zero, LOOP_SUBS3
-    RET
 
     ; Disable signal 0
     LI a2, 0x5 ; 0101
-    SW a2, LCD_CONTROL, t0
+    LI t0, LCD_DATA
+    SB a2, 1[t0]
 
     RET
 
